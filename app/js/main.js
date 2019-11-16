1ed21bb8b1e9b6b80897c0a63d36052dac1bc554
Vladyslav Koziatnyk <wados.wlad@gmail.com>
2019-11-16 15:33:10 +0200

    Go Surf chapter 1-2

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+
+
+$('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt=""></img>',
+    asNavFor: '.slider-dots-header'
+});
+
+$('.slider-dots-header').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
+})
+  
+$('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt=""></img>',
+    asNavFor: '.slider-map'
+});
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true
+});
+
+});
